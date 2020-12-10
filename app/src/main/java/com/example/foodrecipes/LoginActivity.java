c623d32434ce8739de58563ebbde61820379d127
ddn05 <dadannazril@gmail.com>
2020-12-10 16:02:03 +0700

    recipe

---
+++ app/src/main/java/com/example/foodrecipes/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.example.foodrecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private final String USERNAME = "admin";
+    private final String PASSWORD = "admin123";
+
+    private EditText inputUsername;
+    private EditText inputPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        inputUsername = findViewById(R.id.input_username);
+        inputPassword = findViewById(R.id.input_password);
+
+        // autofill
+        inputUsername.setText(USERNAME);
+        inputPassword.setText(PASSWORD);
+
+        getSupportActionBar().setTitle("Login");
+    }
+
+    public void login(View view) {
+        String userUsername = inputUsername.getText().toString();
+        String userPassword = inputPassword.getText().toString();
+
+        if (!userUsername.equalsIgnoreCase(USERNAME) || !userPassword.equals(PASSWORD)) {
+            Toast.makeText(this, "Wrong Login Account", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(this, RecipeCategoryActivity.class);
+        intent.putExtra("username", userUsername);
+        this.startActivity(intent);
+    }
+
+
+}
